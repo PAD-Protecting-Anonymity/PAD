5a85ee793bccd36cb5b075253819db4d1ec42c18
Fisayo Caleb Sangogboye <fsan@tek-cu-mmmi-03.local>
2018-01-10 23:36:25 +0100

    new departure results

--- evaluate/occupancy_statistics_deep.py
+++ evaluate/occupancy_statistics_deep.py
@@ -23,7 +23,7 @@ mel = MetricLearning()
 def evaluation_occupancy_statistics(n, mode = "arrival"):
     day_profile = pd.read_pickle('./dataset/dataframe_all_binary.pkl')
     # day_profile = day_profile[(day_profile.T != 0).any()]
-    day_profile[day_profile>0] = 1
+    # day_profile[day_profile>0] = 1
     
     res = 15
     day_profile = day_profile.iloc[:120,0::res]
@@ -112,7 +112,7 @@ def evaluation_occupancy_statistics(n, mode = "arrival"):
 sanitized = {}
 losses = {}
 sample_sizes = []
-mode = "arrival"
+mode = "departure"
 for n in range(2,8):    
     s, l, ss = evaluation_occupancy_statistics(n, mode)
     sanitized[n] = s
