8ea0e38475d9f4eed57ec8875282ea385bff4006
Fisayo Caleb Sangogboye <fsan@tek-cu-mmmi-03.local>
2018-01-12 01:13:51 +0100

    new figures

--- visualize/generate_figs.py
+++ visualize/generate_figs.py
@@ -7,6 +7,7 @@ files = [("Publication specialized for arrival times with subsampled dataset", "
             ("Publication specialized for departure times with public dataset", "result_scripts/loss_vs_privacy_occupancy_statistics_public_deep_departure.pickle"),
             ("Publication specialized for peak energy usage with subsampled dataset", "result_scripts/loss_vs_privacy_energy_usage_normal_deep.pickle"),
             ("Publication specialized for peak energy usage with public dataset", "result_scripts/loss_vs_privacy_energy_usage_public_deep.pickle"),
-            ]
+            ("Publication specialized for occupancy pattern at Lunch with public dataset", "result_scripts/loss_vs_privacy_occupancy_window_public_deep.pickle"),
+            ("Publication specialized for occupancy pattern at Lunch with subsample dataset", "result_scripts/loss_vs_privacy_occupancy_window_normal_deep.pickle"),]
 for file in files:
     vis.infoloss_vs_level(file[0], file[1])
